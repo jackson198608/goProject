a1e119df566bfa3ad73d71c85801432d44853b32
周鹏远 <zhoupengyuan@goumin.com>
2016-10-15 00:23:11 +0800

    add ios push

--- iosPushDemo/push_test.go
+++ iosPushDemo/push_test.go
@@ -1,10 +1,13 @@
-package main
+package iosPush
 
 import (
-	"fmt"
+	"testing"
 )
 
+const workNum = 10
+
 func work(c chan int) {
+	//redisString format: devicetoken | json
 	var redisString string = `fb71306452499efc778cc77d1be6614b8e1753e79b7acd8348ce6cb47abd4dc2|{"aps":{"alert":"task","sound":"default","badge":1,"type":6,"mark":""}}`
 	t := NewTask(redisString)
 	w := NewWorker(t)
@@ -12,9 +15,13 @@ func work(c chan int) {
 	c <- 1
 }
 
-func main() {
-	c := make(chan int)
-	go work(c)
-	result := <-c
-	fmt.Println(result)
+func TestFibonacci(t *testing.T) {
+	c := make(chan int, workNum)
+
+	for i := 0; i < workNum; i++ {
+		go work(c)
+	}
+	for i := 0; i < workNum; i++ {
+		<-c
+	}
 }
