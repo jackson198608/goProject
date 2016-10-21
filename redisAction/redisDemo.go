6f32700a00ff1f04f79e5570f7663ed5672f02fa
周鹏远 <zhoupengyuan@goumin.com>
2016-10-21 12:37:25 +0800

    test

--- redisAction/redisDemo.go
+++ redisAction/redisDemo.go
@@ -2,32 +2,22 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/redis.v4"
+	redis "gopkg.in/redis.v4"
 )
 
-func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "192.168.86.88:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
-
-	err = client.Set("key", "value", 0).Err()
+func testSet(client *redis.Client) {
+	err := (*client).Set("zhou", "Google", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := client.Get("key").Result()
+	val, err := (*client).Get("zhou").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("key", val)
+	fmt.Println("zhou", val)
 
-	val2, err := client.Get("mykey").Result()
+	val2, err := (*client).Get("mykey").Result()
 	if err == redis.Nil {
 		fmt.Println("mykey does not exists")
 	} else if err != nil {
@@ -35,4 +25,64 @@ func main() {
 	} else {
 		fmt.Println("mykey", val2)
 	}
+}
+
+func testRpush(client *redis.Client) {
+	err := (*client).RPush("list1", "do1").Err()
+	if err != nil {
+		panic(err)
+	}
+	err = (*client).RPush("list1", "do2").Err()
+	if err != nil {
+		panic(err)
+	}
+	err = (*client).RPush("list1", "do3").Err()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		val := (*client).LPop("list1").Val()
+		fmt.Println(val)
+
+		if val == "" {
+			fmt.Println("there is no data2")
+			break
+		}
+	}
+}
+
+func testLRange(client *redis.Client) {
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+	(*client).RPush("list2", "do1")
+}
+
+func conn(conn string) (client *redis.Client) {
+	client = redis.NewClient(&redis.Options{
+		Addr:     conn,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+	return client
+
+}
+
+func main() {
+	client := conn("127.0.0.1:6379")
+	//testSet(client)
+	testRpush(client)
 }
--- testMongo/main.go
+++ testMongo/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"log"
 )
 
-type Person struct {
-	Name  string
-	Phone string
+type Message struct {
+	Uid           int    `json:"uid"`
+	Type          int    `json: "type"`
+	Mark          int    `json: "mark"`
+	Isnew         int    `json: "isnew"`
+	From          int    `json: "from"`
+	Channel       int    `json: "channel"`
+	Channel_types int    `json: "channel_types"`
+	Title         string `json: "title"`
+	Content       string `json: "content"`
+	Image         string `json: "image"`
+	Url_type      int    `json: "url_type"`
+	Url           string `json: "url"`
+	Created       string `json: "created"`
+	Modified      string `json: "modified"`
 }
 
 func main() {
@@ -23,17 +36,25 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
+
+	jsonStr := `{"uid":1895167,"type":1,"makr":281,"isnew":0,"from":0,"channel":1,"channel_types":2,"title":"狗狗的寂寞都市之殇","content":"小短腿在家漂移，屁股差点没甩掉了~","image":"/messagepush/day_161020/20161020_7a50e50.jpg","url_type":1,"url":"4346101","created":"2016-10-20 14:12:28","modified":"0000-00-00 00:00:00"}`
+
+	var m Message
+	if err = json.Unmarshal([]byte(jsonStr), &m); err != nil {
+		fmt.Println("error json fotmat")
+	}
+
+	err = c.Insert(&m)
+	//err = c.Insert(&Message{1895167, 1, 281, 0, 0, 1, 2, "狗狗的寂寞都市之殇", "小短腿在家漂移，屁股差点没甩掉了~", "/messagepush/day_161020/20161020_7a50e50.jpg", 1, "4346101", "2016-10-20 14:12:28", "0000-00-00 00:00:00"})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := Person{}
-	err = c.Find(bson.M{"name": "Cla"}).One(&result)
+	var result Message
+	err = c.Find(bson.M{"uid": 1895167}).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Phone:", result.Phone)
+	fmt.Println("title:", result.Title)
 }
