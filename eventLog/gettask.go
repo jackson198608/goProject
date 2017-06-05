12fa122d49a5025272f7145bbbde154543cf39d2
wang <wanghongxu@goumin.com>
2017-06-05 18:38:45 +0800

    for #task-process update follow array

--- eventLog/gettask.go
+++ eventLog/gettask.go
@@ -71,9 +71,9 @@ func getFollowTask(page int) []string {
 
 	offset := page * c.numloops
 	//获取正常显示和隐藏的数据
-	// sql := "select distinct(user_id) from follow order by id asc limit " + strconv.Itoa(c.numloops) + " offset " + strconv.Itoa(offset)
+	sql := "select distinct(user_id) from follow order by id asc limit " + strconv.Itoa(c.numloops) + " offset " + strconv.Itoa(offset)
 	// sql := "select distinct(user_id) from follow where id <= " + strconv.Itoa(c.followLastId) + " and id >= " + strconv.Itoa(c.followFirstId) + " order by id asc limit " + strconv.Itoa(c.numloops) + " offset " + strconv.Itoa(offset)
-	sql := "select distinct(user_id) from follow where user_id in(1,881050,881052,1138687,49567,1138689,1140002,1140013,1140001,1140009,1139968,1139934,1139976) and id < " + strconv.Itoa(c.followLastId) + " and id >= " + strconv.Itoa(c.followFirstId) + " order by id asc limit " + strconv.Itoa(c.numloops) + " offset " + strconv.Itoa(offset)
+	// sql := "select distinct(user_id) from follow where user_id in(1,881050,881052,1138687,49567,1138689,1140002,1140013,1140001,1140009,1139968,1139934,1139976) and id < " + strconv.Itoa(c.followLastId) + " and id >= " + strconv.Itoa(c.followFirstId) + " order by id asc limit " + strconv.Itoa(c.numloops) + " offset " + strconv.Itoa(offset)
 	logger.Info(sql)
 	rows, err := db.Query(sql)
 
