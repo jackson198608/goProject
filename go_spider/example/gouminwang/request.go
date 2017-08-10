2248443f64225d2f05895a05a015ac59f996e0b4
wang <wanghongxu@goumin.com>
2017-08-10 14:48:39 +0800

    for #gouminwang get weixin article

--- go_spider/example/gouminwang/request.go
+++ go_spider/example/gouminwang/request.go
@@ -8,6 +8,7 @@ import (
 func newRequest(tag string, url string) *request.Request {
 	h := make(http.Header)
 	h.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36")
+	// h.Add("User-Agent", "Sogou web spider/4.0(+http://www.sogou.com/docs/help/webmasters.htm#07)")
 	h.Add("Connection", "close")
 	h.Add("Accept-Encoding", "gzip")
 
@@ -18,6 +19,7 @@ func newRequest(tag string, url string) *request.Request {
 func newImageRequest(tag string, url string) *request.Request {
 	h := make(http.Header)
 	h.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36")
+	// h.Add("User-Agent", "Sogou Pic Spider/3.0(+http://www.sogou.com/docs/help/webmasters.htm#07)")
 	h.Add("Connection", "close")
 	h.Add("Accept-Encoding", "gzip")
 	h.Add("Referer", "http://www.dianping.com/shop/38076482")
