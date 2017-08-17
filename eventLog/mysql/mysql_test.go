7b6e921169d0a313dbb8c075fd3c1f32f23471b5
liyingxue <liyingxue@goumin.com>
2017-08-17 16:29:46 +0800

    event log save to fans,  if fuid is fans ,source = 3

--- eventLog/mysql/mysql_test.go
+++ eventLog/mysql/mysql_test.go
@@ -6,6 +6,19 @@ import (
 	"testing"
 )
 
+func TestCheckIsFans(t *testing.T) {
+	dbName := "test_dz2"
+	db, err := sql.Open("mysql", "root:goumintech@tcp(192.168.86.72:3309)/"+dbName+"?charset=utf8")
+	if err != nil {
+		logger.Error("[error] connect db err")
+	}
+	// uid := 1
+	uid := 1138687
+	follow_id := 1138687
+	fans := CheckIsFans(uid, follow_id, db)
+	logger.Info(fans)
+}
+
 func TestGetFansData(t *testing.T) {
 	dbName := "test_dz2"
 	db, err := sql.Open("mysql", "root:goumintech@tcp(192.168.86.72:3309)/"+dbName+"?charset=utf8")
