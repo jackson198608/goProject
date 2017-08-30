a97f9f05a5fa133b80d7ad163b86bbdbee456e24
wang <wanghongxu@goumin.com>
2017-08-30 14:46:34 +0800

    for #goumincms add template html get content

--- goumincms/mysql.go
+++ goumincms/mysql.go
@@ -40,12 +40,17 @@ type Forum struct {
 	Name       string
 }
 
+type Relatelink struct {
+	Name string
+	Url  string
+}
+
 func LoadThreadByTid(tid int, db *sql.DB) *Thread {
 	tableName := "pre_forum_thread"
 	rows, err := db.Query("select tid,fid,posttableid,typeid,author,authorid,subject,dateline,views,replies from `" + tableName + "` where displayorder in(0,1) and tid=" + strconv.Itoa(int(tid)) + "")
 	defer rows.Close()
 	if err != nil {
-		logger.Error("[error] check event_log sql prepare error: ", err)
+		logger.Error("[error] check pre_forum_thread sql prepare error: ", err)
 		return nil
 	}
 	for rows.Next() {
@@ -56,12 +61,28 @@ func LoadThreadByTid(tid int, db *sql.DB) *Thread {
 	return &Thread{}
 }
 
+func LoadFirstPostByTid(tid int, posttableid int, db *sql.DB) *Post {
+	tableName := "pre_forum_post_" + strconv.Itoa(posttableid)
+	rows, err := db.Query("select pid,tid,first,author,authorid,subject,dateline,message from `" + tableName + "` where invisible=0 and first=1 and tid=" + strconv.Itoa(int(tid)) + "")
+	defer rows.Close()
+	if err != nil {
+		logger.Error("[error] check pre_forum_post first sql prepare error: ", err)
+		return nil
+	}
+	for rows.Next() {
+		var row = new(Post)
+		rows.Scan(&row.Pid, &row.Tid, &row.First, &row.Author, &row.Authorid, &row.Subject, &row.Dateline, &row.Message)
+		return row
+	}
+	return &Post{}
+}
+
 func LoadPostsByTid(tid int, posttableid int, db *sql.DB) []*Post {
 	tableName := "pre_forum_post_" + strconv.Itoa(posttableid)
 	rows, err := db.Query("select pid,tid,first,author,authorid,subject,dateline,message from `" + tableName + "` where invisible=0 and tid=" + strconv.Itoa(int(tid)) + " order by dateline")
 	defer rows.Close()
 	if err != nil {
-		logger.Error("[error] check event_log sql prepare error: ", err)
+		logger.Error("[error] check pre_forum_post sql prepare error: ", err)
 		return nil
 	}
 	var rowsData []*Post
@@ -105,4 +126,21 @@ func LoadForumByFid(fid int, typeid int, db *sql.DB) *Forum {
 	}
 	return &Forum{}
 
+}
+
+func LoadRelateLink(db *sql.DB) []*Relatelink {
+	tableName := "pre_common_relatedlink"
+	rows, err := db.Query("select name,url from `" + tableName + "`")
+	defer rows.Close()
+	if err != nil {
+		logger.Error("[error] check pre_common_relatedlink sql prepare error: ", err)
+		return nil
+	}
+	var rowsData []*Relatelink
+	for rows.Next() {
+		var row = new(Relatelink)
+		rows.Scan(&row.Name, &row.Url)
+		rowsData = append(rowsData, row)
+	}
+	return rowsData
 }
