f260de5f68ccaf1b2dff9b00eb00772508128009
zhoupengyuan <zhoupengyuan@goumin.com>
2017-10-17 11:14:38 +0800

    add status for get html

---
+++ getHtmlProject/HTMLlinkCreater/htmlLinkCreater_test.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+var channelTable map[string]string = map[string]string{
+	"ask":    "ask_question",
+	"thread": "pre_forum_thread",
+}
+
+type HtmlLinkCreater struct {
+	channelType   string
+	jobType       int
+	lastIndexTime string
+	tableName     string
+	tableLastId   int64
+	xorm          *xorm
+}
+
+func NewHtmlLinkCreater(channelType string, jobType int, lastIndexTime string) *HtmlLinkCreater {
+	h := new(HtmlLinkCreater)
+	if h == nil {
+		return nil
+	}
+
+	//check param
+
+	//pass prams to object
+
+	//get tableName and lastId
+	h.tableName = h.getTableNameFromChannel()
+
+	//new xorm instance
+
+	return h
+}
+
+func (h *HtmlLinkCreater) Create() error {
+	buidAll()
+	updateAll()
+}
+
+//
+func (h *HtmlLinkCreater) buidAll() error {
+	//for startid:=0,endId:=100; startid>h.tableLastId ;startid=startid+100,endId=endId+100{
+	h.getIdsFromTableByRange()
+	//}
+}
+
+func (h *HtmlLinkCreater) updateAll() error {
+	//for startid:=0,endId:=100; startid>h.tableLastId ;startid=startid+100,endId=endId+100{
+
+	//}
+}
+
+//
+func (h *HtmlLinkCreater) getIdsFromTableByRange(startid int, endId int) []int {
+	h.xorm.where().find(&pre_forum_thread.Pre_fomrum_thread)
+
+}
+
+//get tableName by oject's channelType from common var channelType
+func (h *HtmlLinkCreater) getTableNameFromChannel() {
+
+}
+
+// get last id of table
+func (h *HtmlLinkCreater) getLastIdFromTable() {
+
+}
---
+++ getHtmlProject/README.md
@@ -0,0 +1,15 @@
+# introduction
+- this package is design for the situation when you want to get the request body save new file using abuyun proxy
+
+# Installation
+
+    go get github.com/jackson198608/goProject/getHtml 
+
+# Run
+
+- run ask
+    getHtml asksave /etc/configask.json all/update  (all: all of data. update:newest 7 days data)
+    getHtml ask /etc/configask.json
+- run thread 
+    getHtml threadsave /etc/configthread.json all/update  (all: all of data. update:newest 7 days data)
+    getHtml thread /etc/configthread.json
