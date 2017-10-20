c7c37bb0ab203ba780eba4b8a0ba87f5d0d24953
liyingxue <liyingxue@goumin.com>
2017-10-20 15:37:39 +0800

    base function

--- pushContentCenter/channels/focus/focus_test.go
+++ pushContentCenter/channels/focus/focus_test.go
@@ -1,1 +1,81 @@
 package focus
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	mgo "gopkg.in/mgo.v2"
+	"testing"
+)
+
+func TestParseJson(t *testing.T) {
+	dbAuth := "dog123:dog123"
+	dbDsn := "210.14.154.117:33068"
+	dbName := "new_dog123"
+	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
+	engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	mongoConn := "192.168.86.192:27017"
+	session, err := mgo.Dial(mongoConn)
+	if err != nil {
+		fmt.Println("[error] connect mongodb err")
+		return
+	}
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+	f := NewFocus(engine, session, jobStr)
+	f.parseJson()
+}
+
+func TestFansPersons(t *testing.T) {
+	dbAuth := "dog123:dog123"
+	// dbDsn := "210.14.154.117:33068"
+	dbDsn := "192.168.86.193:3307"
+	dbName := "new_dog123"
+	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
+	engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	mongoConn := "192.168.86.192:27017"
+	session, err := mgo.Dial(mongoConn)
+	if err != nil {
+		fmt.Println("[error] connect mongodb err")
+		return
+	}
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(engine, session, jobStr)
+	f.getFansPersons(1)
+}
+
+func TestClubPersons(t *testing.T) {
+	dbAuth := "dog123:dog123"
+	dbDsn := "192.168.86.193:3307"
+	dbName := "new_dog123"
+	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
+	engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	mongoConn := "192.168.86.192:27017"
+	session, err := mgo.Dial(mongoConn)
+	if err != nil {
+		fmt.Println("[error] connect mongodb err")
+		return
+	}
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(engine, session, jobStr)
+	f.getClubPersons(1)
+}
