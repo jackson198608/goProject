40f9f38b4f053c9b523e94edb0138aff39a5a637
liyingxue <liyingxue@goumin.com>
2017-10-20 20:46:34 +0800

    focus

--- pushContentCenter/channels/focus/focus_test.go
+++ pushContentCenter/channels/focus/focus_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestParseJson(t *testing.T) {
+func testConn() (*xorm.Engine, *mgo.Session) {
 	dbAuth := "dog123:dog123"
 	dbDsn := "210.14.154.117:33068"
 	dbName := "new_dog123"
@@ -16,90 +16,96 @@ func TestParseJson(t *testing.T) {
 	engine, err := xorm.NewEngine("mysql", dataSourceName)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, nil
 	}
 
 	mongoConn := "192.168.86.192:27017"
 	session, err := mgo.Dial(mongoConn)
 	if err != nil {
 		fmt.Println("[error] connect mongodb err")
-		return
+		return nil, nil
 	}
+	return engine, session
+}
 
+func TestParseJson(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
 	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
-	f := NewFocus(engine, session, jobStr)
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
 	f.parseJson()
 }
 
 func TestFansPersons(t *testing.T) {
-	dbAuth := "dog123:dog123"
-	// dbDsn := "210.14.154.117:33068"
-	dbDsn := "192.168.86.193:3307"
-	dbName := "new_dog123"
-	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
-	engine, err := xorm.NewEngine("mysql", dataSourceName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	mongoConn := "192.168.86.192:27017"
-	session, err := mgo.Dial(mongoConn)
-	if err != nil {
-		fmt.Println("[error] connect mongodb err")
-		return
-	}
+	mysqlXorm, mongoConn := testConn()
 
 	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
 
-	f := NewFocus(engine, session, jobStr)
-	f.getFansPersons(1)
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	f.getFansPersons(1, 10)
 }
 
 func TestClubPersons(t *testing.T) {
-	dbAuth := "dog123:dog123"
-	dbDsn := "192.168.86.193:3307"
-	dbName := "new_dog123"
-	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
-	engine, err := xorm.NewEngine("mysql", dataSourceName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	mongoConn := "192.168.86.192:27017"
-	session, err := mgo.Dial(mongoConn)
-	if err != nil {
-		fmt.Println("[error] connect mongodb err")
-		return
-	}
+	mysqlXorm, mongoConn := testConn()
 
 	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
 
-	f := NewFocus(engine, session, jobStr)
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
 	f.getClubPersons(1)
 }
 
-func TestBreedPersons(t *testing.T) {
-	dbAuth := "dog123:dog123"
-	dbDsn := "192.168.86.193:3307"
-	dbName := "new_dog123"
-	dataSourceName := dbAuth + "@tcp(" + dbDsn + ")/" + dbName + "?charset=utf8mb4"
-	engine, err := xorm.NewEngine("mysql", dataSourceName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	mongoConn := "192.168.86.192:27017"
-	session, err := mgo.Dial(mongoConn)
-	if err != nil {
-		fmt.Println("[error] connect mongodb err")
-		return
-	}
+func TestGetClubPersonPageNum(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
 
 	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
 
-	f := NewFocus(engine, session, jobStr)
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	page := f.getClubPersonPageNum()
+	fmt.Println(page)
+}
+
+func TestGetBreedPersonsPagNum(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	page := f.getBreedPersonsPagNum()
+	fmt.Println(page)
+}
+
+func TestBreedPersons(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
 	f.getBreedPersons(1)
+}
+
+func TestFansPersonFirstId(t *testing.T) {
+
+	mysqlXorm, mongoConn := testConn()
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	f.getFansPersonFirstId()
+}
+
+func TestFansPersonLastId(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
+
+	jobStr := "{\"uid\":2060500,\"event_type\":2,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	f.getFansPersonLastId()
+}
+
+func TestGetPersons(t *testing.T) {
+	mysqlXorm, mongoConn := testConn()
+
+	jobStr := "{\"uid\":881050,\"event_type\":6,\"event_info\":{\"title\":\"subject\",\"content\":\"message\",\"image_num\":\"image_num\",\"forum\":\"forum->name\",\"tag\":0,\"source\":2,\"fid\":36,\"bid\":34},\"tid\":0,\"status\":1,\"time\":1508469600}|1|0"
+
+	f := NewFocus(mysqlXorm, mongoConn, jobStr)
+	f.getPersons(0, 1, 999999999)
 }
