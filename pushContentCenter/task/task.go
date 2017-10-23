09f7c068ab897e5b7295007f1275829e7249e536
周鹏远 <zhoupengyuan@goumin.com>
2017-10-23 09:04:26 +0800

    add task.go

--- pushContentCenter/task/task.go
+++ pushContentCenter/task/task.go
@@ -1,58 +1,106 @@
 package task
 
 import (
+	"errors"
 	"fmt"
 	"github.com/go-xorm/xorm"
 	"github.com/jackson198608/goProject/pushContentCenter/channels/club"
 	"github.com/jackson198608/goProject/pushContentCenter/channels/focus"
 	mgo "gopkg.in/mgo.v2"
+	"strings"
 )
 
 type Task struct {
-	raw       string
-	mysqlXorm *xorm.Engine
-	mongoConn *mgo.Session
-	jobstr    string
-	jobType   string
+	Raw       string       //the data get from redis queue
+	MysqlXorm *xorm.Engine //mysql single instance
+	MongoConn *mgo.Session //mongo single instance
+	Jobstr    string       //private member parse from raw
+	JobType   string       //private membe parse from raw jobType: focus|club
 }
 
 //job: redisQueue pop string
 //taskarg: mongoHost,mongoDatabase,mongoReplicaSetName
-func NewTask(raw string, mysqlXorm *xorm.Engine, mongoConn *mgo.Session) *task {
-	//@todo check prams
+func NewTask(raw string, mysqlXorm *xorm.Engine, mongoConn *mgo.Session) *Task {
+	//check prams
+	if (raw == "") || (mysqlXorm == nil) || (mongoConn == nil) {
+		return nil
+	}
 
-	t = new(Task)
+	t := new(Task)
 	if t == nil {
 		return nil
 	}
 
-	//@todo pass param
+	//pass params
+	t.Raw = raw
+	t.MysqlXorm = mysqlXorm
+	t.MongoConn = mongoConn
 
-	//@todo create private member
+	//create private member
 	jobStr, jobType, err := t.parseRaw()
 	if err != nil {
 		return nil
 	}
-	//@todo check return detail
-
-	t.jobstr = jobStr
-	t.jobType = jobType
 
 	return t
 
 }
 
+// public interface for task
+// if you have New channles you need to add logic here
 func (t *Task) Do() error {
+	switch t.JobType {
+	case "club":
+		err := t.ChannelClub()
+		if err != nil {
+			return err
+		} else {
+			return nil
+		}
+	case "focus":
+		err := t.ChannelFocus()
+		if err != nil {
+			return err
+		} else {
+			return nil
+		}
 
+	}
 	return nil
 }
 
+// focus channel's invoke function
+func (t *Task) ChannelFocus() error {
+	c := focus.NewFocus(t.MysqlXorm, t.MongoConn, t.Jobstr)
+	err := c.Do()
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+// club channel's invoke function
+func (t *Task) ChannelClub() error {
+	return nil
+
+}
+
+// this function parase raw to judge jobstr and job type
+// sep string : '|'
 //return:
 //         jobstr
 //	       type
-//         trytimes
 //		   error
-func (t *Tasl) parseRaw() (string, string, error) {
+func (t *Task) parseRaw() error {
+	rawArray = strings.Split(t.Raw, "|")
+	if len(rawArray) < 2 {
+		return errors.New("job str format error ", t.Raw)
+	}
 
-	return "", "", 0, nil
+	t.Jobstr = rawArray[0]
+	t.JobType = rawArray[1]
+
+	return nil
+
 }
