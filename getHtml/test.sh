d6be9d231868dab633ab74e7870ad46dc81ed762
zhoupengyuan <zhoupengyuan@goumin.com>
2018-03-02 17:31:07 +0800

    rm unseless code

--- getHtml/test.sh
+++
@@ -1,6 +0,0 @@
-work/src$ go install github.com/jackson198608/goProject/getHtml
-work/src$ ../bin/getHtml thread "/etc/goumincmsnew.json"
-work/src$ ../bin/getHtml createnum "/etc/goumincmsnew.json"
-
-work/src$ ../bin/getHtml ask "/etc/goumincmsask.json"
-work/src$ ../bin/getHtml createnum "/etc/goumincmsask.json"
