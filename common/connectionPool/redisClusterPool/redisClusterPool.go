21c009c68b2a417a2e9c5d01de39484a3d6d68ba
wang <wanghongxu@goumin.com>
2018-05-04 18:50:02 +0800

    for #task-redis-pool

--- common/connectionPool/redisClusterPool/redisClusterPool.go
+++ common/connectionPool/redisClusterPool/redisClusterPool.go
@@ -2,28 +2,31 @@ package redisClusterPool
 
 import (
 	"errors"
+	"fmt"
 	queue "github.com/oleiade/lane"
 	redis "gopkg.in/redis.v4"
+	"gouminGitlab/common/tools"
+	"reflect"
 	"sync"
 )
 
 type redisPool struct {
-	redisInfo redis.ClusterOptions
+	redisInfo *redis.ClusterOptions
 	num       int
 	poolQueue queue.Queue
 	lock      sync.RWMutex
 }
 
-func NewRedisPool(redisInfo redis.ClusterOptions, num int) (redisPool, error) {
+func NewRedisPool(redisInfo *redis.ClusterOptions, num int) (*redisPool, error) {
 	//@todo check params
 	redisPool := new(redisPool)
 	if redisPool == nil {
-		return nil, errors.New("create memory error.can not create it")
+		return redisPool, errors.New("create memory error.can not create it")
 	}
-
+	redisPool.redisInfo = redisInfo
 	err := redisPool.initPool()
 	if err != nil {
-		return nil, err
+		return redisPool, err
 	}
 
 	return redisPool, nil
@@ -33,40 +36,50 @@ func NewRedisPool(redisInfo redis.ClusterOptions, num int) (redisPool, error) {
 func (r *redisPool) initPool() error {
 	for i := 0; i < r.num; i++ {
 		connection, err := r.createOneConnection()
+		fmt.Println(i, connection, reflect.TypeOf(connection))
 		if err != nil {
 			//@todo something
 		} else {
-			r.poolQueue.Enqueue()
+			r.poolQueue.Enqueue(connection)
 		}
 	}
+	return nil
 }
 
 func (r *redisPool) createOneConnection() (*redis.ClusterClient, error) {
-
+	client, err := tools.GetClusterClient(r.redisInfo)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
 
 func (r *redisPool) Close() error {
 	for i := 0; i < r.poolQueue.Size(); i++ {
 		connection := r.poolQueue.Dequeue()
+		connection.(*redis.ClusterClient).Close()
 		//@todo close
 	}
+	return nil
 }
 
 func (r *redisPool) GetConnection() (*redis.ClusterClient, error) {
 	r.lock.Lock()
 	defer r.lock.RLock()
+	fmt.Println("get connection ", r)
 	if r.poolQueue.Size() == 0 {
 		return nil, errors.New("there is no more connection can be use ,please wait")
-	} else {
-		connection := r.poolQueue.Dequeue()
-		return connection, nil
 	}
+	connection := r.poolQueue.Dequeue()
+	fmt.Println("get connection ", connection)
+	return connection.(*redis.ClusterClient), nil
 
 }
 
 func (r *redisPool) PutConnection(connection *redis.ClusterClient) error {
 
 	r.lock.Lock()
+
 	r.poolQueue.Enqueue(connection)
 	r.lock.RLock()
 	return nil
--- common/connectionPool/redisClusterPool/redisClusterPool_test.go
+++ common/connectionPool/redisClusterPool/redisClusterPool_test.go
@@ -1,16 +1,20 @@
 package redisClusterPool
 
 import (
+	"fmt"
+	"gouminGitlab/common/tools"
 	"testing"
 	"time"
 )
 
 const conum = 100
 
-var redisClusterPool *redisClusterPool
+var redisClusterPool *redisPool
+var redisConn = "192.168.86.193:6380,192.168.86.193:6381,192.168.86.193:6382,192.168.86.193:6383,192.168.86.193:6384,192.168.86.193:6385"
 
 func TestIndex(t *testing.T) {
 	redisClusterPool = createPool()
+	// fmt.Println(redisClusterPool)
 	for i := 0; i < 100; i++ {
 		go work(i)
 	}
@@ -18,18 +22,33 @@ func TestIndex(t *testing.T) {
 
 func work(i int) {
 	for {
-		connection, error := redisClusterPool.GetConnection()
+		fmt.Println("i:", i)
+		connection, err := redisClusterPool.GetConnection()
+		fmt.Println("connection:", connection)
+		if err != nil {
+			fmt.Println("work redis connection err", err)
+		}
 		if connection == nil {
 			time.Sleep(10 * time.Microsecond)
 			continue
 		} else {
-			connection.lpush()
+			// connection.lpush()
+			_, err := (*connection).LPush("messageDatas", "messageData").Result()
+			if err != nil {
+				fmt.Println("lpush err", err)
+			}
 			redisClusterPool.PutConnection(connection)
 		}
 	}
 }
 
-func createPool() *redisClusterPool {
-	redisClusterPool := NewRedisPool()
+func createPool() *redisPool {
+	redisInfo := tools.FormatRedisOption(redisConn)
+	redisClusterPool, err := NewRedisPool(&redisInfo, conum)
+	if err != nil {
+		fmt.Println("new redis pool err", err)
+		return redisClusterPool
+		//to do
+	}
 	return redisClusterPool
 }
