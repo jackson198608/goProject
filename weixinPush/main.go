e69f478fef8a3da7be4e942f365a79e36f665d59
周鹏远 <zhoupengyuan@goumin.com>
2018-06-09 12:15:56 +0800

    change logic of weixinPush

--- weixinPush/main.go
+++ weixinPush/main.go
@@ -10,14 +10,14 @@ import (
 	redis "gopkg.in/redis.v4"
 	//"gouminGitlab/common/weixin/accessToken/accesstokenManager"
 	//"gouminGitlab/common/weixin/accessToken"
+	"encoding/json"
+	"fmt"
+	"github.com/jackson198608/goProject/common/http/abuyunHttpClient"
+	"net/http"
+	"strconv"
 	"strings"
 	"sync"
-	"net/http"
-	"encoding/json"
 	"time"
-	"github.com/jackson198608/goProject/common/http/abuyunHttpClient"
-	"fmt"
-	"strconv"
 )
 
 var c Config = Config{
@@ -25,7 +25,7 @@ var c Config = Config{
 	1,                //thread num
 	"weixinPush",     //queuename
 	"appSecret",      // app and secret of every program
-	"workTime",      //工作时间，该时间段内取任务
+	"workTime",       //工作时间，该时间段内取任务
 }
 
 //var weixinAccessTokens * accesstokenManager.Manager
@@ -70,6 +70,14 @@ func main() {
 
 func jobFuc(job string, redisConn *redis.ClusterClient, mysqlConns []*xorm.Engine, mgoConns []*mgo.Session, taskarg []string) error {
 
+	//check if it is work time
+	for {
+		if isWorkTime() {
+			break
+		}
+		time.Sleep(10 * time.Minute)
+	}
+
 	t, err := task.NewTask(job, mysqlConns, mgoConns)
 	if err != nil {
 		return err
@@ -82,28 +90,21 @@ func jobFuc(job string, redisConn *redis.ClusterClient, mysqlConns []*xorm.Engin
 		return err
 	}
 
-	for {
-		if isWorkTime(){
-			break
-		}
-		time.Sleep(10*time.Minute)
-	}
-
 	return err
 }
 
-func isWorkTime() bool{
+func isWorkTime() bool {
 	//该时间段内不发送任务
 	work := c.workTime
 
 	rawSlice := []byte(work)
 	rawLen := len(rawSlice)
 	lastIndex := strings.LastIndex(work, "|")
-	start,err := strconv.Atoi(string(rawSlice[0:lastIndex]))
+	start, err := strconv.Atoi(string(rawSlice[0:lastIndex]))
 	if err != nil {
 		start = int(8)
 	}
-	end,err1 := strconv.Atoi(string(rawSlice[lastIndex+1 : rawLen]))
+	end, err1 := strconv.Atoi(string(rawSlice[lastIndex+1 : rawLen]))
 	if err1 != nil {
 		end = int(22)
 	}
@@ -121,37 +122,37 @@ func getToken(appid string, redisConn *redis.ClusterClient) string {
 	var token string
 
 	//check it the token exist
-	if(!redisConn.Exists(key).Val()){
+	if !redisConn.Exists(key).Val() {
 		w.Do(func() {
-			token = generateToken(appid,redisConn)
+			token = generateToken(appid, redisConn)
 		})
-	} else{
-		token=redisConn.Get(key).Val()
+	} else {
+		token = redisConn.Get(key).Val()
 	}
 
 	return token
 }
 
-func generateToken(appid string,redisConn *redis.ClusterClient) string {
+func generateToken(appid string, redisConn *redis.ClusterClient) string {
 	//@todo
 	//using appid to search var appids to find the secret
 	secret := appids[appid]
 	//using appid and secret to get token and set back into redis
-	target := _tokenUrl+"appid="+appid+"&secret="+secret
+	target := _tokenUrl + "appid=" + appid + "&secret=" + secret
 	var h http.Header = make(http.Header)
 	abuyun := getAbuyun()
 	defer abuyun.Close()
-	statusCode, _, body, err := abuyun.SendRequest(target,h,"",true)
+	statusCode, _, body, err := abuyun.SendRequest(target, h, "", true)
 	if err != nil {
 		return err.Error()
 	}
 	if statusCode == 200 {
 		var result map[string]interface{}
-		if err:=json.Unmarshal([]byte(body),&result);err==nil{  //解析json，获取accesstoken
-			token := fmt.Sprintf("%s",result["access_token"]) //access_token默认2个小时的有效期
+		if err := json.Unmarshal([]byte(body), &result); err == nil { //解析json，获取accesstoken
+			token := fmt.Sprintf("%s", result["access_token"]) //access_token默认2个小时的有效期
 			//更新缓存
-			key := accesstoken_key+appid
-			redisConn.Set(key,token,time.Duration((7200-20) *time.Second))
+			key := accesstoken_key + appid
+			redisConn.Set(key, token, time.Duration((7200-20)*time.Second))
 			return token
 		}
 	}
