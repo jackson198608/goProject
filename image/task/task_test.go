c6c7f0e2e1a45bc22d86f521140cffa4650f7f5a
liyingxue <liyingxue@goumin.com>
2018-11-30 18:02:13 +0800

    image add log , check file size is 0 ,try again

--- image/task/task_test.go
+++ image/task/task_test.go
@@ -28,7 +28,7 @@ func TestDo(t *testing.T) {
 	taskarg = append(taskarg, "127.0.0.1")
 	taskarg = append(taskarg, "")
 	//jobStr := "{\"path\":\"/Users/Snow/img/data/attachment/forum/201802/12/1518412896316.png\",\"width\":200,\"height\":200,\"callback\":\"compressnotify/updatepromotiongoods\",\"args\":\"1\",\"watermark\":\"/Users/Snow/img/1.png\",\"gravity\":\"northwest\",\"x\":0,\"y\":0}|all"
-	jobStr   := "{\"path\": \"/Users/Snow/img/IMG_03001.JPG\",\"width\": 800,\"height\": 800,\"callback\": \"compressnotify/updatepromotiongoods\",\"args\": {\"p_id\": 6726,\"pg_id\": 42029,\"goods_id\": \"5244\",\"sku_id\": \"19421\",\"module\": 3,\"url\": \"/goodsSku/day_181106/20181106_f097c48.jpg\",\"width\": 800,\"size\": 3},\"watermark\": \"/Users/Snow/img/1.png\",\"gravity\": \"northwest\",\"x\": 0,\"y\": 0,\"afterPath\":\"/Users/Snow/img/IMG_03001_2222.JPG\"}|composite"
+	jobStr   := "{\"path\": \"/Users/Snow/img/IMG_03001.JPG\",\"width\": 800,\"height\": 800,\"callback\": \"compressnotify/updatepromotiongoods\",\"args\": {\"p_id\": 6726,\"pg_id\": 42029,\"goods_id\": \"5244\",\"sku_id\": \"19421\",\"module\": 3,\"url\": \"/goodsSku/day_181106/20181106_f097c48.jpg\",\"width\": 800,\"size\": 3},\"watermark\": \"/Users/Snow/img/1.png\",\"gravity\": \"northwest\",\"x\": 0,\"y\": 0,\"afterPath\":\"/Users/Snow/img/IMG_03001_2222.JPG\"}|all"
 	f, _ := NewTask(jobStr, taskarg)
 	fmt.Println(f.Do())
 }
