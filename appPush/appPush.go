f842276d20c31020f9057103ce1978436a82db16
wangjinfeng <wangjinfeng@goumin.com>
2019-05-28 13:56:42 +0800

    android push改为mob推送

--- appPush/appPush.go
+++ appPush/appPush.go
@@ -8,13 +8,27 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/thinkboy/log4go"
 )
 
 //gloabl variables
 var timeout time.Duration = 5
 
+//mobPush推送
+var mobKey = "2b14bb6c10bac"
+var mobSign = "172cca337b3b9ea4cf4b250bd7c773e0"
+
 type Worker struct {
 	t *Task
+}
+
+type modPushRes struct {
+	Status int `json:"status"`
+	Error  string `json:"error"`
+	Res    string `json:"res"`
 }
 
 func Init(t time.Duration) {
@@ -33,7 +47,8 @@ func (w Worker) Push(p12bytes []byte) (result bool) {
 	if phoneType == 0 {
 		result = w.iosPush(p12bytes)
 	} else if phoneType == 1 {
-		result = w.androidPush()
+		//result = w.androidPush()
+		result = w.androidPushMob()
 	} else {
 		//wx program
 		result = w.wxProgramPush()
@@ -100,4 +115,49 @@ func (w Worker) androidPush() (result bool) {
 	ioutil.ReadAll(resp.Body)
 	fmt.Println("[notice] android response Body:", string(body))
 	return true
+}
+
+//android push 新的推送方法
+func (w Worker) androidPushMob() (result bool) {
+	url := "http://api.push.mob.com/v2/push"
+
+	var jsonStr []byte = []byte(w.t.TaskJson)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	//sign加密
+	info := w.t.TaskJson + mobSign
+	secret := md5Str(info)
+
+	req.Header.Set("key", mobKey)
+	req.Header.Set("sign", secret)
+	req.Close = true
+
+	timeout := time.Duration(timeout * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		result = false
+		return result
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	ioutil.ReadAll(resp.Body)
+
+	var a modPushRes
+	json.Unmarshal([]byte(string(body)),&a)
+	if a.Status != 200 {
+		log4go.Debug("android push fail status:",a.Status," error:",a.Error," taskJson:",w.t.TaskJson)
+	}
+	fmt.Println("[notice] android response Body:", string(body))
+
+	return true
+}
+//md5加密
+func md5Str(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
